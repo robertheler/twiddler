2df751170b1e05e1d4c896efdd5d2adec980ff22
Robert Heler <robertheler@gmail.com>
2020-04-17 22:20:23 -0700

    Latest

--- data_generator.js
+++ data_generator.js
@@ -18,7 +18,7 @@ window.users = Object.keys(streams.users);
 // utility function for adding tweets to our data structures
 var addTweet = function(newTweet){
   var username = newTweet.user;
-  
+
     streams.users[username].push(newTweet);
     streams.home.push(newTweet);
     $('.img').click();
